78258bf1f898f4d9696ee8f748aa28df653c0ce4
danidedona <daniela_dedona@brown.edu>
2024-02-27 15:15:55 -0500

    fix: switching verbose and brief now works yet it is now in REPLInput

--- mock/src/components/REPLFunction.tsx
+++ mock/src/components/REPLFunction.tsx
@@ -1,7 +1,7 @@
 import { Dispatch, SetStateAction, useState } from "react";
 /**
- * A command-processor function for our REPL. The function returns a string, which is the value to print to history when
- * the command is done executing.
+ * A command-processor function for our REPL. The function returns a string,
+ * which is the value to print to history when the command is done executing.
  *
  * The arguments passed in the input (which need not be named "args") should
  * *NOT* contain the command-name prefix.
@@ -9,7 +9,7 @@ import { Dispatch, SetStateAction, useState } from "react";
 
 export interface REPLFunction {
   (args: Array<string>): string;
-  setVerbose: Dispatch<SetStateAction<boolean>>;
+  //setVerbose: Dispatch<SetStateAction<boolean>>;
   //execute(): (args: Array<string>) => string;
   //call: (args: Array<string>) => string | string[][];
 }
